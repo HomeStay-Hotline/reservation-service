ae9f18737727feca65ee04bdb16f871f685cd2a6
Andrew Song <andrewsong61@gmail.com>
2020-11-11 17:35:03 -0800

    Create React boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        Hey lol from react
+      </div>
+    )
+  }
+}
+
+export default App;
