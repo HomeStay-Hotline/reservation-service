73d4d02544933f743fd518079bad5f5343b22a90
Andrew Song <andrewsong61@gmail.com>
2020-11-13 10:47:10 -0800

    Add first API call

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const parser = require('body-parser');
 const path = require('path');
+const db = require('../db/index');
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,19 @@ app.use(morgan('dev'));
 app.use(parser.json());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+app.get('/api/homes/:id/calendar', (req, res) => {
+  const id = req.params.id
+
+  db.Listing.find({ listing_ID: id }, 'dates', (err, results) => {
+    if (err) {
+      console.log('Failed to fetch dates from the database: ', err);
+      res.sendStatus(500);
+    } else {
+      res.send(results);
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server running on http:localhost:${port}`);
