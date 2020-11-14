683b744f3b60a9b85fd29ca0ee328f01ca12b59f
Andrew Song <andrewsong61@gmail.com>
2020-11-13 21:07:55 -0800

    Write test for the Calendar component

--- test/components/Calendar.test.js
+++ test/components/Calendar.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import Calendar from '../../client/components/Calendar';
+import CalendarHeader from '../../client/components/CalendarHeader';
+import CalendarBody from '../../client/components/CalendarBody';
+import CalendarFooter from '../../client/components/CalendarFooter';
 
-describe('Calendar Testing', () => {
-  test('it should render the calendar header', () => {
+describe('<Calendar />', () => {
+  test('it should render the calendar with three Calendar-related components', () => {
     const wrapper = shallow(<Calendar />);
-    expect(wrapper.find('.header')).toHaveLength(2);
+    expect(wrapper.containsAllMatchingElements([
+      <CalendarHeader />,
+      <CalendarBody />,
+      <CalendarFooter />,
+    ]));
   });
 });
