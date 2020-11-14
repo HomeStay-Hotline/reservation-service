b80643e489ee1cfa8795cc7c61901f527f4ab5e5
Andrew Song <andrewsong61@gmail.com>
2020-11-13 17:54:59 -0800

    Rename component to reflect its purpose

---
+++ client/components/Calendar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        hey
+      </div>
+    );
+  }
+}
+
+export default Calendar;
