fcee545c748a7f04d3557aa9076e3cc1bfe8df35
Andrew Song <andrewsong61@gmail.com>
2020-11-14 17:44:30 -0800

    Refactor Calendar; fetch data from server

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,22 +1,45 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
-import CalendarHeader from './CalendarHeader';
+import axios from 'axios';
 import CalendarBody from './CalendarBody';
-import CalendarFooter from './CalendarFooter';
+import styles from '../../public/styles/calendar.css';
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      dates: [],
     };
   }
 
+  componentDidMount() {
+    axios.get('/api/homes/1/calendar')
+      .then(({ data }) => {
+        this.setState({
+          dates: data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  clearDates() {
+    console.log('Clearing dates...');
+  }
+
   render() {
+    const { dates } = this.state;
     return (
       <div className="container">
-        <CalendarHeader />
-        <CalendarBody />
-        <CalendarFooter />
+        <div className={styles.header}>
+          <h1>Select check-in date</h1>
+          <h3>Add your travel dates for exact pricing</h3>
+        </div>
+        <CalendarBody dates={dates} />
+        <div className={styles.footer}>
+          <button type="submit" onClick={this.clearDates.bind(this)}>Clear dates</button>
+        </div>
       </div>
     );
   }
