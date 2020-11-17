8379aba29725ad998a29ecc8fc35068cac4f0b6b
Andrew Song <andrewsong61@gmail.com>
2020-11-16 17:39:07 -0800

    Modify stylesheet

--- client/components/CalendarBody.jsx
+++ client/components/CalendarBody.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Month from './Month';
-import styles from '../../public/styles/calendar.css';
+import styles from '../../public/styles/CalendarBody.css';
 
 const CalendarBody = (props) => {
   const { dates } = props;
