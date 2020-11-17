9e57dc616a9fd238b2ed3e317efc6a2ad912a6c0
Andrew Song <andrewsong61@gmail.com>
2020-11-16 16:32:14 -0800

    Convert to functional component with Hooks

--- client/components/CalendarBody.jsx
+++ client/components/CalendarBody.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Month from './Month';
 import styles from '../../public/styles/calendar.css';
 
-class CalendarBody extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentMonths: ['November 2020', 'December 2020'],
-    };
-  }
-
-  render() {
-    const { currentMonths } = this.state;
-    return (
-      <div className={styles.body}>
-        <div className={styles.bodyLeft}>
-          <Month month={currentMonths[0]} left />
-        </div>
-        <div className={styles.bodyRight}>
-          <Month month={currentMonths[1]} />
-        </div>
+const CalendarBody = (props) => {
+  const { dates } = props;
+  // const handleLeftRightToggle = () => null;
+  return (
+    <div className={styles.body}>
+      <div className={styles.bodyLeft}>
+        <Month monthArr={dates[0]} left />
       </div>
-    );
-  }
-}
+      <div className={styles.bodyRight}>
+        <Month monthArr={dates[1]} />
+      </div>
+    </div>
+  );
+};
+
+// Set dates' propType to be an array of arrays containing date objects of the described shape
+CalendarBody.propTypes = {
+  dates: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({
+    dayOfWeek: PropTypes.string.isRequired,
+    month: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    available: PropTypes.bool.isRequired,
+  }))).isRequired,
+};
 
 export default CalendarBody;
