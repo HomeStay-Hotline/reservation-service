035baa9c6adc636bca0dc4cec8ecc2deb7c549e1
Andrew Song <andrewsong61@gmail.com>
2020-11-17 14:13:34 -0800

    Refactor state and props to best implement check in functionality

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -5,7 +5,10 @@ import CalendarBody from './CalendarBody';
 import styles from '../../public/styles/Calendar.css';
 
 const Calendar = () => {
+  const [listing, setListing] = useState({});
   const [dates, setDates] = useState([]);
+  const [checkInDate, setCheckInDate] = useState({});
+  const [checkInClicked, setcheckInClicked] = useState(false);
 
   // fetch dates data from the server
   // empty array as 2nd arg stops fetch from infinite loop
@@ -18,6 +21,9 @@ const Calendar = () => {
           monthsWithDates.push(data[0].dates.filter((date) => date.month === months[i]));
         }
         setDates(monthsWithDates);
+        delete data[0].dates;
+        console.log(data[0]);
+        setListing(data[0]);
       })
       .catch((err) => {
         console.log(err);
@@ -28,13 +34,49 @@ const Calendar = () => {
     console.log('Clearing dates...');
   };
 
+  const handleCheckInSelect = (date) => {
+    console.log('Check-in date selected!', date);
+    setcheckInClicked(true);
+    setCheckInDate(date);
+  };
+
+  if (checkInClicked) {
+    return (
+      <div className="container">
+        <div className={styles.header}>
+          <h1>Select check-out date</h1>
+          <h3>
+            Minimum stay:
+            {' '}
+            {listing.minDays}
+            {' '}
+            nights
+          </h3>
+        </div>
+        <CalendarBody
+          dates={dates}
+          checkInDate={checkInDate}
+          checkInClicked={checkInClicked}
+          listing={listing}
+          handleCheckInSelect={handleCheckInSelect}
+        />
+        <div className={styles.footer}>
+          <button type="submit" onClick={clearDates}>Clear dates</button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container">
       <div className={styles.header}>
         <h1>Select check-in date</h1>
         <h3>Add your travel dates for exact pricing</h3>
       </div>
-      <CalendarBody dates={dates} />
+      <CalendarBody
+        dates={dates}
+        listing={listing}
+        handleCheckInSelect={handleCheckInSelect}
+      />
       <div className={styles.footer}>
         <button type="submit" onClick={clearDates}>Clear dates</button>
       </div>
