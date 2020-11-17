a477f83b94cdc9846864e7233e881b92ebc6d954
Andrew Song <andrewsong61@gmail.com>
2020-11-16 16:31:51 -0800

    Convert to functional component with Hooks

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,48 +1,45 @@
 /* eslint-disable class-methods-use-this */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CalendarBody from './CalendarBody';
 import styles from '../../public/styles/calendar.css';
 
-class Calendar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dates: [],
-    };
-  }
+const Calendar = () => {
+  const [dates, setDates] = useState([]);
 
-  componentDidMount() {
+  // fetch dates data from the server
+  // empty array as 2nd arg stops fetch from infinite loop
+  useEffect(() => {
     axios.get('/api/homes/1/calendar')
       .then(({ data }) => {
-        this.setState({
-          dates: data,
-        });
+        const months = ['November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'];
+        const monthsWithDates = [];
+        for (let i = 0; i < months.length; i += 1) {
+          monthsWithDates.push(data[0].dates.filter((date) => date.month === months[i]));
+        }
+        setDates(monthsWithDates);
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  }, []);
 
-  clearDates() {
+  const clearDates = () => {
     console.log('Clearing dates...');
-  }
+  };
 
-  render() {
-    const { dates } = this.state;
-    return (
-      <div className="container">
-        <div className={styles.header}>
-          <h1>Select check-in date</h1>
-          <h3>Add your travel dates for exact pricing</h3>
-        </div>
-        <CalendarBody dates={dates} />
-        <div className={styles.footer}>
-          <button type="submit" onClick={this.clearDates.bind(this)}>Clear dates</button>
-        </div>
+  return (
+    <div className="container">
+      <div className={styles.header}>
+        <h1>Select check-in date</h1>
+        <h3>Add your travel dates for exact pricing</h3>
       </div>
-    );
-  }
-}
+      <CalendarBody dates={dates} />
+      <div className={styles.footer}>
+        <button type="submit" onClick={clearDates}>Clear dates</button>
+      </div>
+    </div>
+  );
+};
 
 export default Calendar;
