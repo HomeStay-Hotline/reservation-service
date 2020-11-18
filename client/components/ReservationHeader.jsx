eaa3f470d586e3250717bd9410e53f46ccc39a39
Andrew Song <andrewsong61@gmail.com>
2020-11-18 14:22:03 -0800

    Create new components for abstraction

---
+++ client/components/ReservationHeader.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+import styles from '../../public/styles/Reservation.css';
+
+const ReservationHeader = (props) => {
+  const { rate } = props;
+  return (
+    <div className={styles.header}>
+      <div className={styles.ratePerNight}>
+        <div className={styles.rate}>
+          <span>
+            <h1>
+              {'$'}
+              {rate}
+              {' '}
+            </h1>
+          </span>
+        </div>
+        <div className={styles.night}>
+          <span>
+            <h3>/ night</h3>
+          </span>
+        </div>
+      </div>
+      <div className={styles.reviews}>
+        <span>
+          <FontAwesomeIcon icon={faStar} className={styles.icon} />
+        </span>
+        <span>
+          <h1>4.0</h1>
+        </span>
+        <span>
+          <h3>(4)</h3>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+ReservationHeader.propTypes = {
+  rate: PropTypes.number.isRequired,
+};
+
+export default ReservationHeader;
