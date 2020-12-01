54199e0b14a15aa244b28562782c8ccd0bab1367
Andrew Song <andrewsong61@gmail.com>
2020-12-01 10:19:57 -0800

    Update month

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -19,7 +19,7 @@ const Calendar = () => {
   useEffect(() => {
     axios.get(`/api/homes${window.location.pathname}calendar`)
       .then(({ data }) => {
-        const months = ['November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'];
+        const months = ['December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November'];
         const monthsWithDates = [];
         for (let i = 0; i < months.length; i += 1) {
           monthsWithDates.push(data[0].dates.filter((dt) => dt.month === months[i]));
